e67f2274742184531ebd5f79f2f6214cd0efb8b7
goodnamefordog <2443841996@qq.com>
2025-03-07 20:48:47 +0800

    Declare optimize mtu api

--- tinc-game/UHMTUFrame.cpp
+++ tinc-game/UHMTUFrame.cpp
@@ -4,15 +4,20 @@
 UHMTUFrame::UHMTUFrame(wxButton* parentButton) : wxFrame(nullptr, wxID_ANY, frameTitle) {
 	_parentButton = parentButton;
 
-	Bind(wxEVT_CLOSE_WINDOW, &UHMTUFrame::OnClose, this);
+	Bind(wxEVT_CLOSE_WINDOW, &UHMTUFrame::UI_OnClose, this);
+	UI_CreateControls();
 }
-
-void UHMTUFrame::OnClose(wxCloseEvent& event)
+void UHMTUFrame::UI_OnClose(wxCloseEvent& event)
 {
 	if (_parentButton != nullptr) {
 		_parentButton->Enable(true);
 	}
-    event.Skip();
+	event.Skip();
+}
+
+void UHMTUFrame::UI_CreateControls()
+{
+	wxStaticText* staticText = new wxStaticText(panel, wxID_ANY, "Hello, this is a static text!", wxPoint(20, 20));
 }
 
 const std::wstring UHMTUFrame::UHMTUFrame::frameTitle = L"优化MTU";
--- tinc-game/UHMTUFrame.h
+++ tinc-game/UHMTUFrame.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <wx/wx.h>
 
 class UHMTUFrame : public wxFrame
@@ -8,5 +8,19 @@ public:
 private:
 	static const std::wstring frameTitle;
 	wxButton* _parentButton = nullptr;
-	void OnClose(wxCloseEvent& event);
+	void UI_OnClose(wxCloseEvent& event);
+
+	void UI_CreateControls();
+
+	wxPanel* panel = new wxPanel(this);
+
+	// UI to SRV
+	bool API_CheckAddressFormat(std::wstring ipAddress);
+	void API_StartMeasureMTU(std::wstring ipAddress);
+
+	// SRV to UI
+	void API_ReportStatus(std::wstring status);
+	void API_ReportMTU_IPv4(int mtu);
+	void API_ReportMTU_IPv6(int mtu);
+	void API_EndMeasureMTU(bool success, std::wstring reason);
 };
