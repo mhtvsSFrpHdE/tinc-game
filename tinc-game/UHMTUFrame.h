0da15a155d129ddbcd8a0536d6926b1f12a39e48
mhtvsSFrpHdE <2bemperor47@gmail.com>
2025-03-08 18:19:50 +0800

    Update optimize mtu api

--- tinc-game/UHMTUFrame.h
+++ tinc-game/UHMTUFrame.h
@@ -5,6 +5,16 @@ class UHMTUFrame : public wxFrame
 {
 public:
 	UHMTUFrame(wxButton* parentButton);
+
+	// UI to SRV
+	static bool API_CheckAddressFormat(std::wstring ipAddress);
+	void API_StartMeasureMTU(std::wstring ipAddress);
+
+	// SRV to UI
+	void API_ReportStatus(std::wstring status);
+	void API_ReportMTU_IPv4(int mtu);
+	void API_ReportMTU_IPv6(int mtu);
+	void API_EndMeasureMTU(bool success, std::wstring reason);
 private:
 	static const std::wstring frameTitle;
 	wxButton* _parentButton = nullptr;
@@ -13,14 +23,4 @@ private:
 	void UI_CreateControls();
 
 	wxPanel* panel = new wxPanel(this);
-
-	// UI to SRV
-	bool API_CheckAddressFormat(std::wstring ipAddress);
-	void API_StartMeasureMTU(std::wstring ipAddress);
-
-	// SRV to UI
-	void API_ReportStatus(std::wstring status);
-	void API_ReportMTU_IPv4(int mtu);
-	void API_ReportMTU_IPv6(int mtu);
-	void API_EndMeasureMTU(bool success, std::wstring reason);
 };
