fbf50705211a88fcb6055c7c3c17bdc8c9c01857
goodnamefordog <2443841996@qq.com>
2025-04-20 22:00:07 +0800

    Per frame translate file

---
+++ tinc-game/GenerateTranslatePot.bat
@@ -0,0 +1,5 @@
+cd /d "%~dp0"
+xgettext -d tinc-game -s --keyword=_ -p ./po -o tinc-game-App.pot App.cpp
+xgettext -d tinc-game -s --keyword=_ -p ./po -o tinc-game-MainFrame.pot MainFrame.cpp
+xgettext -d tinc-game -s --keyword=_ -p ./po -o tinc-game-UHMTUFrame.pot UHMTUFrame.cpp
+xgettext -d tinc-game -s --keyword=_ -p ./po -o tinc-game-SettingsFrame.pot SettingsFrame.cpp
