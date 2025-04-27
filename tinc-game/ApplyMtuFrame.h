02f81e3eecd6a0b4b4e1b33d79cd5819f7af076c
goodnamefordog <2443841996@qq.com>
2025-04-27 19:17:16 +0800

    Declare apply mtu api

---
+++ tinc-game/ApplyMtuFrame.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <wx/wx.h>
+#include "ReturnValue.h"
+#include <vector>
+#include <string>
+
+struct ApplyMtuResult {
+	enum Enum {
+		ApplyMtu_Other,
+		ApplyMtu_InvalidAdapterName,
+		ApplyMtu_Failed_IPv4,
+		ApplyMtu_Failed_IPv6,
+		ApplyMtu_Failed_All
+	};
+	Enum messageEnum;
+	std::wstring messageString;
+};
+
+class ApplyMtuFrame : public wxFrame
+{
+public:
+	ApplyMtuFrame();
+
+	// UI to SRV
+	static ReturnValue<std::vector<wxString>> API_SRV_GetNetworkAdapterList();
+	static ReturnValue<ApplyMtuResult> API_SRV_ApplyMtu(int mtu_IPv4, int mtu_IPv6, std::wstring adapterName);
+	static bool API_SRV_OpenNetworkControlPanel();
+};
---
+++ tinc-game/ApplyMtuFrame_SRV.cpp
@@ -0,0 +1,30 @@
+#include "ApplyMtuFrame.h"
+
+ApplyMtuFrame::ApplyMtuFrame()
+{
+}
+
+ReturnValue<std::vector<wxString>> ApplyMtuFrame::API_SRV_GetNetworkAdapterList()
+{
+    // TODO
+    // Return false if network adapter list has 0 available network adapter
+    // Return true if any available network adapter founded
+    return ReturnValue<std::vector<wxString>>();
+}
+
+ReturnValue<ApplyMtuResult> ApplyMtuFrame::API_SRV_ApplyMtu(int mtu_IPv4, int mtu_IPv6, std::wstring adapterName)
+{
+    // TODO
+    // Return false if "Apply MTU failed on adapter's IPv4", report error message from netsh
+    // Return true if "All success", "IPv4 success but IPv6 failed"
+    auto what = ReturnValue<ApplyMtuResult>();
+    what.success = false;
+    what.returnBody.messageEnum = ApplyMtuResult::ApplyMtu_Failed_IPv4;
+    what.returnBody.messageString = L"";
+    return ReturnValue<ApplyMtuResult>();
+}
+
+bool ApplyMtuFrame::API_SRV_OpenNetworkControlPanel()
+{
+    return false;
+}
