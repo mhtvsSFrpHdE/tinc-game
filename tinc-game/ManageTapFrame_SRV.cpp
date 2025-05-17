28f898fd5daf81f734ccbda393f0f53252cfc2ae
mhtvsSFrpHdE <2bemperor47@gmail.com>
2025-05-17 10:21:33 +0800

    Move API_SRV to new file

---
+++ tinc-game/ManageTapFrame_SRV.cpp
@@ -0,0 +1,31 @@
+#include "ManageTapFrame.h"
+#include "String_SRV.h"
+
+ReturnValue<std::vector<WindowsAPI_SRV::GetAdaptersAddressesResult>> ManageTapFrame::API_SRV_GetNetworkAdapterList()
+{
+    auto result = ReturnValue<std::vector<WindowsAPI_SRV::GetAdaptersAddressesResult>>();
+
+    WindowsAPI_SRV::GetAdaptersAddresses(&result.returnBody);
+
+    if (result.returnBody.size() > 0) {
+        result.success = true;
+    }
+
+    return result;
+}
+
+ReturnValue<std::wstring> ManageTapFrame::API_SRV_InstallTap()
+{
+    auto result = ReturnValue<std::wstring>();
+    result.success = false;
+    result.returnBody = L"Unknown";
+    return result;
+}
+
+ReturnValue<std::wstring> ManageTapFrame::API_SRV_UninstallTap(WindowsAPI_SRV::GetAdaptersAddressesResult adapter)
+{
+    auto result = ReturnValue<std::wstring>();
+    result.returnBody = adapter.friendlyName + " | " + adapter.modelName + String_SRV::newLine + adapter.windows_LUID;
+    result.success = true;
+    return result;
+}
