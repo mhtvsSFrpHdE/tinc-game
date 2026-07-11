f93fae5a19d79d2d93b21c19bee55c2585d5c104
mhtvsSFrpHdE <2bemperor47@gmail.com>
2026-07-12 01:27:02 +0800

    Update publishDockerImage.bat

--- server/docker/publishDockerImage.bat
+++ server/docker/publishDockerImage.bat
@@ -2,11 +2,13 @@
 @REM -f specify non default dockerfile name to build
 @REM --build-arg pass exist environment variable to builder
 
+set accountName=<docker account name>
+
 docker build ^
 --build-arg HTTP_PROXY=%HTTP_PROXY% ^
 --build-arg HTTPS_PROXY=%HTTPS_PROXY% ^
 --build-arg BUILD_CPU_CORE=%BUILD_CPU_CORE% ^
 --build-arg TINC_GAME_SERVER_HTTPS=false ^
--t jtfeejflcdxj/tinc-game-server:latest .
+-t %accountName%/tinc-game-server:latest .
 
-docker push <account name>/tinc-game-server:latest
+docker push %accountName%/tinc-game-server:latest
